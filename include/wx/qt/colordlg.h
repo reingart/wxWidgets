3ff4831d6202e8880964e51bc956d5a27a380093
Sean D'Epagnier <sean@depagnier.com>
2014-07-29 19:27:18 +0800

    implement wxColourDialog

--- include/wx/qt/colordlg.h
+++ include/wx/qt/colordlg.h
@@ -1,7 +1,7 @@
 /////////////////////////////////////////////////////////////////////////////
 // Name:        wx/qt/colordlg.h
-// Author:      Peter Most
-// Copyright:   (c) Peter Most
+// Author:      Sean D'Epagnier
+// Copyright:   (c) Sean D'Epagnier 2014
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
 
@@ -9,21 +9,25 @@
 #define _WX_QT_COLORDLG_H_
 
 #include "wx/dialog.h"
-#include "wx/cmndata.h"
+
 #include <QtWidgets/QColorDialog>
 
 class WXDLLIMPEXP_CORE wxColourDialog : public wxDialog
 {
 public:
-    wxColourDialog();
+    wxColourDialog() { }
     wxColourDialog(wxWindow *parent,
-                   wxColourData *data = NULL);
+                   wxColourData *data = NULL) { Create(parent, data); }
+    virtual ~wxColourDialog() {}
 
     bool Create(wxWindow *parent, wxColourData *data = NULL);
 
     wxColourData &GetColourData();
 
-    virtual int ShowModal();
+private:
+    QColorDialog *GetQColorDialog() const { return static_cast<QColorDialog *>(m_qtWindow); }
+
+    static wxColourData s_data;
 };
 
 #endif // _WX_QT_COLORDLG_H_
--- src/qt/colordlg.cpp
+++ src/qt/colordlg.cpp
@@ -1,40 +1,49 @@
 /////////////////////////////////////////////////////////////////////////////
 // Name:        src/qt/colordlg.cpp
-// Author:      Peter Most
-// Copyright:   (c) Peter Most
+// Author:      Sean D'Epagnier
+// Copyright:   (c) Sean D'Epagnier 2014
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
 
 // For compilers that support precompilation, includes "wx.h".
 #include "wx/wxprec.h"
 
+#include "wx/qt/private/winevent.h"
 #include "wx/colordlg.h"
 
-wxColourDialog::wxColourDialog()
+class wxQtColorDialog : public wxQtEventSignalHandler< QColorDialog, wxDialog >
 {
-}
+public:
+    wxQtColorDialog( wxWindow *parent, wxDialog *handler)
+        : wxQtEventSignalHandler(parent, handler)
+        { }
+};
 
-wxColourDialog::wxColourDialog(wxWindow *parent, wxColourData *data )
-{
-}
-
+wxColourData wxColourDialog::s_data;
 
 bool wxColourDialog::Create(wxWindow *parent, wxColourData *data )
 {
-    return false;
-}
+    m_qtWindow = new wxQtColorDialog( parent, this );
 
+    if(data)
+        s_data = *data;
+
+    if(s_data.GetChooseFull())
+        for(int i=0; i<wxColourData::NUM_CUSTOM; i++)
+            QColorDialog::setCustomColor(i, s_data.GetCustomColour(i).GetHandle());
+
+    GetQColorDialog()->setCurrentColor(s_data.GetColour().GetHandle());
+
+    return wxTopLevelWindow::Create( parent, wxID_ANY, "Color Dialog");
+}
 
 wxColourData &wxColourDialog::GetColourData()
 {
-    static wxColourData data;
+    for(int i=0; i<wxColourData::NUM_CUSTOM; i++)
+        s_data.SetCustomColour(i, GetQColorDialog()->customColor(i));
     
-    return data;
-}
-
-
-int wxColourDialog::ShowModal()
-{
-    return 0;
+    s_data.SetColour(GetQColorDialog()->currentColor());
+    
+    return s_data;
 }
 
